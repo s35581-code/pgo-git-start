c07d246aa18ba3bdcce837ffaca24f0eae647dda
s35581-code <s35581@pjwstk.edu.pl>
2026-03-24 00:44:55 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,29 @@
 public class Main {} 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Book book1 = new Book("1984", "George Orwell", 328, true);
+
+        Book book2 = new Book("The Hobbit", "J.R.R. Tolkien", 310, true);
+
+        Book book3 = new Book("Clean Code", "Robert C. Martin", 464, false);
+
+        book1.printInfo();
+
+        book2.printInfo();
+
+        book3.printInfo();
+
+        book1.borrow();
+
+        book1.printInfo();
+
+        book1.returnBook();
+
+        book1.printInfo();
+
+    }
+
+}
+
